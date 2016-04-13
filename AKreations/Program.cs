3c34a7d6ef7b9cae70fa242fb6a3180d20064dd4
Aparna Mahadevan <aparna.devan@live.com>
2016-04-12 18:48:25 -0700

    Menu updates

--- AKreations/Program.cs
+++ AKreations/Program.cs
@@ -10,18 +10,39 @@ namespace AKreations
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter your name : ");
+            var Intro = Console.ReadLine();
             Console.WriteLine("+++++++++*************** Welcome to Aparna Kreations ***************+++++++++");
             Console.WriteLine();
             Console.WriteLine();
-            Console.Write("What is the Category for this Item? ");
-            var prodCatogory = Console.ReadLine();
-            Console.Write("What is the name of this Item? ");
-            var prodItemName = Console.ReadLine();
-            Console.Write("Describe this item: ");
-            var prodDesc = Console.ReadLine();
-            var product1 = Store.AddProduct(prodCatogory, prodItemName, 200, 2, prodDesc);
-            Console.WriteLine("Item Category Name: {0}, Item Name: {1}, Item Amount: {2:C}, Item Quantity: {3}, Describition: {4} ", prodCatogory, prodItemName, product1.Amount, product1.Quantity, prodDesc);
-
+            Console.WriteLine("Hello "+ Intro+" ,welcome please select from these options...");
+            Console.WriteLine("1. Add a new product");
+            Console.WriteLine("2. Sell a product");
+            Console.WriteLine("3. Delete a product");
+            Console.WriteLine("0. Exit");
+            var ProdOption = Console.ReadLine();
+            switch (ProdOption)
+            {
+                case "1":
+                    Console.Write("What is the Category for this Item? ");
+                    var prodCatogory = Console.ReadLine();
+                    Console.Write("What is the name of this Item? ");
+                    var prodItemName = Console.ReadLine();
+                    Console.Write("Describe this item: ");
+                    var prodDesc = Console.ReadLine();
+                    var product1 = Store.AddProduct(prodCatogory, prodItemName, 200, 2, prodDesc);
+                    Console.WriteLine("Item Category Name: {0}, Item Name: {1}, Item Amount: {2:C}, Item Quantity: {3}, Describition: {4} ", prodCatogory, prodItemName, product1.Amount, product1.Quantity, prodDesc);
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "0":
+                    Console.WriteLine("Good Bye!");
+                    return;
+                default:
+                    break;
+            }
         }
     }
 }
